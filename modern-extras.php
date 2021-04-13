a30f235afc1f3bf93d72a94a8fbb837f60161175
Diego F. Prado <goidesdesign@gmail.com>
2021-04-13 11:35:50 -0500

    create modern-extra.php

---
+++ modern-extras.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Modern Talent Hub Extra
+ * Plugin URI: https://github.com/goids/moderplugin
+ * Description: Plugin for load libraries for web site
+ * Version: 1.0
+ * Author: Goide
+ */
