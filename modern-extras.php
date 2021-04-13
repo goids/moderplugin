19fdbbd74cb1115f582a436dcfe51b7bfde09d0b
Diego F. Prado <goidesdesign@gmail.com>
2021-04-13 12:02:01 -0500

    create modern-extra.phpDD

--- modern-extras.php
+++ modern-extras.php
@@ -11,5 +11,5 @@ add_action('wp_enqueue_scripts', 'moderntalenthub_enqueue_custom_js');
 function moderntalenthub_enqueue_custom_js() {
     wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js', array(), false, true);
     wp_enqueue_script('gsapTextPlugin', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/TextPlugin.min.js', array(), false, true);
-    wp_enqueue_script('gsapTextPlugin', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/EasePack.min.js', array(), false, true);
+    wp_enqueue_script('gsapEasePack', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/EasePack.min.js', array(), false, true);
 }
